93defc6abfc7aa7daccf45d5e8acc9a9fb45a239
nikajorika <n_jorjoliani@cu.edu.ge>
2013-12-07 23:46:31 +0400

    My fists commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<form method="post" action='index.php'>
+		<table><tr>
+		<td><label>Firstname:</label></td>
+		<td><input class='inputtext' type='text' name='firstname' placeholder='Firstname' /></td></tr>
+		<tr><td><label>Lastname:</label></td>
+		<td><input  class='inputtext' type='text' name='lastname' placeholder='Lastname'/></td></tr>
+
+		<tr><td><label>Username:</label></td>
+		<td><input  class='inputtext' type='text' name='username' placeholder='Username' /></td></tr>
+	    <tr><td><label>Password:</label></td>
+		<td><input class='inputtext'  type='password' name='password' placeholder='Password' /></td></tr>
+		<tr><td><label>Confirm Password:</label></td>
+		<td><input class='inputtext'  type='password' name='password-confirmation' placeholder='Confirm Password' /></td></tr>
+		<tr><td><label>E-mail:</label></td>
+		<td><input class='inputtext'  type='text' name='email' placeholder='E-mail' /></td></tr>
+		<tr><td><label>Date Of Birth:</label></td>
+		<td><input class='inputtext' id='date-field' type='date' name='day-of-bearth'  /></td></tr>
+		<tr><td><label>Gender:</label></td>
+		<td><select name='gender' id='gender-field' class='inputtext'>
+			<option  value='none' slected='1' >None</option>
+			<option  value='M'  >Male</option>
+			<option  value='F'  >Female</option>
+		</select>
+			<td></tr>
+		<tr><td colspan='2' align='center'><input type='submit' name='submit' id='registration-button' value='Register' /></td></tr>
+
+
+</table>
+
+	</form>
+
+
+
+
+
+</body>
+</html>
